f52d3420e32050dafe255a932ca8b6171063d8d4
Gustavo Santos Melo <gsantos15569@gmail.com>
2022-01-22 01:16:13 -0300

    User controller

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,84 @@
+<?php
+namespace App\Http\Controllers;
+
+use Exception;
+use App\Models\UserModel;
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends BaseController {
+    /**
+     * @var UserModel
+     */
+    private $userModel;
+
+    /**
+     * @param UserModel $userModel
+     */
+    public function __construct(UserModel $userModel) {
+        $this->userModel = $userModel;
+
+        parent::__construct($this->userModel);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function store(Request $request): JsonResponse
+    {
+        try {
+            $password_hash = Hash::make($request->input('password'));
+
+            $this->userModel->first_name = ucfirst($request->input('firstName'));
+            $this->userModel->last_name = ucfirst($request->input('lastName'));
+            $this->userModel->email = $request->input('email');
+            $this->userModel->password = $password_hash;
+
+            $this->userModel->save();
+
+            return response()->json([
+                'message' => 'User created with success'
+            ]);
+        } catch (Exception $err) {
+            return throw new Exception('Error to create a user');
+        }
+    }
+
+
+    /**
+     * @inheritDoc
+     */
+    public function update(Request $request, int $id): JsonResponse
+    {
+        $user = $this->userModel::find($id);
+
+        if (empty($user)) {
+            return throw new Exception('Cannot update this user');
+        }
+
+        $password = $request->input('password');
+        $checkPassword = $request->input('checkPassword');
+        $firstName = $request->input('firstName');
+        $lastName = $request->input('lastName');
+        $email = $request->input('email');
+
+        if (empty($password)) {
+            return throw new Exception('Cannot update this user');
+        } else if (Hash::check($password, $user->password) && $password === $checkPassword) {
+            try {
+
+                $user->firstName = (empty($firstName)) ? $user->firstName : $firstName;
+                $user->lastName = (empty($lastName)) ? $user->lastName : $lastName;
+                $user->email = (empty($email)) ? $user->email : $email;
+
+                $user->save();
+                return response()->json([
+                    'message' => 'User updated with success'
+                ]);
+            } catch (Exception $err) {
+                return throw new Exception('Error to update this user, try again');
+            }
+        }
+    }
+}
