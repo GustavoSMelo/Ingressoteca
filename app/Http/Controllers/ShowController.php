16ca50691448c6a98b5c1191ec5357b908f0a7e3
Gustavo Santos Melo <gsantos15569@gmail.com>
2022-01-22 01:16:48 -0300

    Show controller

---
+++ app/Http/Controllers/ShowController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ShowModel;
+use DomainException;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class ShowController extends BaseController
+{
+    /**
+     * @var ShowModel
+     */
+    private $showModel;
+
+    /**
+     * @param ShowModel $showModel
+     */
+    public function __construct(ShowModel $showModel)
+    {
+        $this->showModel = $showModel;
+
+        parent::__construct($this->showModel);
+    }
+
+    /**
+     * Method to get all show per one category
+     *
+     * @param string $category
+     * @return JsonResponse
+     */
+    public function getPerCategory(Request $request, string $category): JsonResponse
+    {
+        try {
+            $showsWithCategory = $this->showModel->where('category', $category)->get();
+
+            if (count($showsWithCategory) <= 0) {
+                return throw new DomainException('Does not exist any show with this category');
+            }
+
+            $offset = ($request->page - 1) * 5;
+
+            $paginate = $showsWithCategory->query()
+                ->offset($offset)
+                ->limit(5)
+                ->get();
+
+            return response()->json($paginate);
+        } catch (DomainException $err) {
+            return throw new DomainException('error to get shows with this category');
+        }
+    }
+}
