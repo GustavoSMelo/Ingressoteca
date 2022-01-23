721244dd5426f8282caa981f206581a398d2ff5b
Gustavo Santos Melo <gsantos15569@gmail.com>
2022-01-22 22:07:27 -0300

    new routes in application

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('client.index');
+});
+
+Route::get('/create/user/account', function () {
+    return view('client.create_user_account');
 });
