b98bb2ad6550c42c1b362c7f10c6c7d1ab41beed
Gustavo Santos Melo <gsantos15569@gmail.com>
2022-01-22 22:07:14 -0300

    template layout

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <title>Ingressoteca</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <link rel="stylesheet" type="text/css" href="/css/global.css" />
+        @yield('css')
+        @stack('scripts')
+    </head>
+    <body>
+        @yield('content')
+    </body>
+</html>
