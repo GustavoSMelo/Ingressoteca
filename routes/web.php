0988f029133e500c0c636a177727b39831ffda38
Gustavo Santos Melo <gsantos15569@gmail.com>
2022-01-24 02:16:38 -0300

    routes for web application

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('client.index');
-});
+Route::get('/', fn () =>
+    view('client.index')
+);
 
-Route::get('/create/user/account', function () {
-    return view('client.create_user_account');
-});
+Route::get('/create/user/account', fn () =>
+    view('client.create_user_account')
+);
+
+Route::get('/user/login', fn () =>
+    view('client.login_user')
+);
+
+Route::get('/home', fn () =>
+    view('client.home')
+);
