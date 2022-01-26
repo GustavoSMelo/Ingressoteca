bef942c46ee70b22130409aef8e00946fa990d90
Gustavo Santos Melo <gsantos15569@gmail.com>
2022-01-26 01:11:15 -0300

    update api routes

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\TokenController;
 use App\Http\Controllers\UserController;
 use App\Models\ShowModel;
 use Illuminate\Support\Facades\Route;
+use Psy\Command\ShowCommand;
 
 Route::prefix('/ticket')->group(function () {
     Route::post('', [TicketController::class, 'store']);
@@ -17,15 +18,19 @@ Route::prefix('/ticket')->group(function () {
 Route::prefix('/purchase')->group(function () {
     Route::post('', [PurchaseController::class, 'store']);
     Route::delete('/{id}', [PurchaseController::class, 'delete']);
+    Route::get('/{id}', [PurchaseController::class, 'show']);
 });
 
 Route::prefix('/user')->group(function () {
     Route::post('/login', [TokenController::class, 'generateToken']);
+    Route::get('/{id}', [UserController::class, 'show']);
     Route::post('', [UserController::class, 'store']);
 });
 
 Route::prefix('/announcer')->group(function () {
     Route::post('', [AnnouncerController::class, 'store']);
+    Route::post('/login', [AnnouncerController::class, 'login']);
+    Route::get('/{id}', [AnnouncerController::class, 'show']);
 });
 
 Route::prefix('/show')->group(function () {
@@ -34,5 +39,6 @@ Route::prefix('/show')->group(function () {
     Route::get('/{id}', [ShowController::class, 'show']);
     Route::delete('/{id}', [ShowController::class, 'delete']);
     Route::put('/{id}', [ShowController::class, 'store']);
-    Route::get('/{category}', [ShowController::class, 'getPerCategory']);
+    Route::get('/category/{category}', [ShowController::class, 'getPerCategory']);
+    Route::get('/announcer/{id}', [ShowController::class, 'getByAnnouncerId']);
 });
