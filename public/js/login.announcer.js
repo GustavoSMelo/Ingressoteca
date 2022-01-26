0d37713074a69a26fff36318b74a54df31bd6f6c
Gustavo Santos Melo <gsantos15569@gmail.com>
2022-01-26 01:14:56 -0300

    logic to login with announcer

---
+++ public/js/login.announcer.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const btnLogin = window.document.querySelector('#btnLogin');
+const APP_URL = 'http://localhost:8000';
+
+btnLogin.addEventListener('click', () => {
+    const email = window.document.querySelector('#email').value;
+    const password = window.document.querySelector('#password').value;
+
+    const requestInfo = {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            email,
+            password
+        })
+    };
+
+    fetch(`${APP_URL}/api/announcer/login`, requestInfo)
+        .then(response => {
+            if (!response.ok){
+                const boxError = window.document.querySelector('.error-message');
+                boxError.style.display = 'block';
+            }
+
+            return response.json();
+        })
+        .then(data => {
+            localStorage.setItem('Authorization', `Bearer ${data.token}`);
+            localStorage.setItem('Aid', data.id);
+
+            window.document.location = `${APP_URL}/admin/home`;
+        })
+        .catch(error => {
+            console.error(error);
+            const boxError = window.document.querySelector('.error-message');
+            boxError.style.display = 'block';
+        });
+});
