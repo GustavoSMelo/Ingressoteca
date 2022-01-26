9d07213f27aa177a659b916cee0cf2664f70bc8e
Gustavo Santos Melo <gsantos15569@gmail.com>
2022-01-26 01:11:05 -0300

    update user profile

--- resources/views/client/user_profile.blade.php
+++ resources/views/client/user_profile.blade.php
@@ -1,7 +1,24 @@
 @extends('layout')
 
 @section('css')
+    <link rel="stylesheet" type="text/css" href="/css/user.profile.css"/>
 @endsection
 
 @section('content')
+    <main>
+        <section>
+            <figure>
+                <img src="/images/user.png" alt="user icon" />
+            </figure>
+            <span class="user-information">
+            </span>
+        </section>
+
+        <article>
+            <h2>Tickets</h2>
+            <table class="tickets">
+            </table>
+        </article>
+    </main>
+    <script type="text/javascript" src="{{asset('js/user.profile.js')}}" ></script>
 @endsection
